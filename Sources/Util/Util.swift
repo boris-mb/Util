47845d82e6da657f8e1c87350e6c0a8ac8e4d046
Boris Möbus <boris.moebus@gmx.de>
2020-05-05 11:40:19 +0200

    initial. func decode added to Bundle

--- Sources/Util/Util.swift
+++ Sources/Util/Util.swift
@@ -1,3 +1,18 @@
-struct Util {
-    var text = "Hello, World!"
+
+import Foundation
+
+extension Bundle {
+   func decode<T:Codable>( _ file: String) ->[T] {
+      guard let url = self.url(forResource: file, withExtension: nil) else {
+         fatalError("Failed to locate \(file) in bundle.")
+      }
+      guard let data = try? Data(contentsOf: url) else {
+         fatalError("Failed to read \(file) data ")
+      }
+      let decoder = JSONDecoder()
+      guard let loaded = try? decoder.decode( [T].self, from: data) else {
+         fatalError("Failed to decode \(file)")
+      }
+      return loaded
+   }
 }
--- Tests/UtilTests/UtilTests.swift
+++ Tests/UtilTests/UtilTests.swift
@@ -6,7 +6,7 @@ final class UtilTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Util().text, "Hello, World!")
+        // XCTAssertEqual(Util().text, "Hello, World!")
     }
 
     static var allTests = [
